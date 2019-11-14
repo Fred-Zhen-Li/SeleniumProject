c30771aa2c82ec37043d7b31cc485604ccab4659
lz921 <921252641@qq.com>
2019-11-14 20:48:31 +0800

    A Simple Selenium auto test script.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
-}
+        public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","D:\\BrowserDriver\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+            Thread.sleep(1000);
+            driver.get("http://www.baidu.com");
+            Thread.sleep(2000);
+            String title = driver.getTitle();
+            System.out.print(title);
+            Thread.sleep(1000);
+            driver.close();
+        }
+    }
