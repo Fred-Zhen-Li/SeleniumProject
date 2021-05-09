783fc886c56064b8248157ce1b4f495d3602bb62
921252641@qq.com <lz92125>
2021-05-09 22:43:04 +0800

    Create a Java Table

--- src/main/java/Jave_Test.java
+++ src/main/java/Jave_Test.java
@@ -1,2 +1,7 @@
 public class Jave_Test {
-}
+    public static void main(String[] args) {
+        int[] Table1 = new int[]{1, 2, 3, 4, 5};
+        for (int i = 0; i < Table1.length; i++)
+            System.out.println(Table1[i]);
+        }
+    }
